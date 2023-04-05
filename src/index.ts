311898a7e127777c1a911156ccaa3807539637bc
DmytroSoldatov <cyberdexx@gmail.com>
2023-04-05 08:41:39 +0300

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference, spaced-comment
+/// <reference path="./utils/Global.d.ts" />
+
 export * from './controllers/ContentController';
 export * from './controllers/SizeController';
 export * from './controllers/StyleController';
@@ -5,3 +8,4 @@ export * from './controllers/align/AlignController';
 export * from './utils/constants';
 export * from './utils/types';
 export * from './Layout';
+export * from './utils/transformContainer';
