c5d68372a4bbead7d78e436ecb722078d5ddc295
Dmytro Soldatov <cyberdexx@gmail.com>
2023-10-18 12:12:42 +0300

    fix basic story

--- src/stories/basic/Basic.stories.ts
+++ src/stories/basic/Basic.stories.ts
@@ -42,28 +42,20 @@ class LayoutStory
     {
         this.layout = new Layout({
             id: 'root',
-            content: {
-                text: {
-                    content: LOREM_TEXT,
-                    styles: {
-                        color,
-                        textAlign,
-                        fontSize,
-                        wordWrap,
-                        width: '100%',
-                        height: '100%',
-                    }
-                }
-            },
+            content: LOREM_TEXT,
             styles: {
                 background: backgroundColor,
                 width: `${width}%`,
                 height: `${height}%`,
-                padding,
                 opacity,
                 overflow,
                 position,
                 borderRadius,
+                color,
+                textAlign,
+                fontSize,
+                wordWrap,
+                padding,
             }
         });
 
